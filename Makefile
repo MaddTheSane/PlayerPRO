b57c245c18a49bfb3f3ad9302bf3574010b68501
C.W. Betts <computers57@hotmail.com>
2010-02-15 05:42:20 +0000

    Case-sensitive fixes. Adding a few more files. I don't know why I updated autogen to work with both OS X and Linux, but I did.
    git-svn-id: svn://svn.code.sf.net/p/playerpro/code/trunk@103 f4605116-7794-4d0b-9e48-65ca9bfeedfd

--- Makefile
+++ Makefile
@@ -2,8 +2,6 @@ OS=$(shell uname -s)
 ifeq ($(OS), Darwin)
 include Makefile-Mac
 else
-
-
-
+# TODO: check for configure and Makefile-other and run neccesary tools to make them
 include Makefile-other
 endif
--- autogen.sh
+++ autogen.sh
@@ -5,9 +5,15 @@ echo "This may take a while ..."
 
 # Regenerate configuration files.
 
+OS=$(uname -s)
 aclocal
 # autoheader
-automake --include-deps --add-missing 
+automake --include-deps --add-missing --copy
+if test ${OS} = "Darwin"; then
+glibtoolize
+else
+libtoolize
+fi
 autoconf
 
 echo "Now you are ready to run ./configure."
