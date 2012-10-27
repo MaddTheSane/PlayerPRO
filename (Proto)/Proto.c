ed89ad84598757838ed347693ba8461da184f9b2
C.W. Betts <computers57@hotmail.com>
2012-10-27 23:11:14 +0000

    Making Proto.c include PPPrivate, RDriver, and RDriverInt headers
    git-svn-id: svn://svn.code.sf.net/p/playerpro/code/branches/PlayerPRO5@255 f4605116-7794-4d0b-9e48-65ca9bfeedfd

--- (Proto)/Proto.c
+++ (Proto)/Proto.c
@@ -1,7 +1,10 @@
 #include <Carbon/Carbon.h>
 #include <CoreFoundation/CFPlugInCOM.h>
 #include "MAD.h"
+#include "RDriver.h"
+#include "RDriverInt.h"
 #include "PPPlug.h"
+#include "PPPrivate.h"
 
 #ifdef __OBJC__
 #include <Cocoa/Cocoa.h>
