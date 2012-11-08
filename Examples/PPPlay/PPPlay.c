97e66295f6579ee4a2b1645c25382e527a9935c6
C.W. Betts <computers57@hotmail.com>
2012-11-08 20:48:13 +0000

    Making a command-line PlayerPRO player.
    git-svn-id: svn://svn.code.sf.net/p/playerpro/code/branches/PlayerPRO6@354 f4605116-7794-4d0b-9e48-65ca9bfeedfd

---
+++ Examples/PPPlay/PPPlay.c
@@ -0,0 +1,15 @@
+
+#ifdef __APPLE__
+#include <PlayerPROCore/PlayerPROCore.h>
+#else
+#include "RDriver.h"
+#include "FileUtils.h"
+#endif
+
+int main(int argc, char* argv[])
+{
+	if(argc != 2)
+	{
+		return EXIT_FAILURE;
+	}
+}
