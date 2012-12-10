82bea8a6ef92ff02ab7c019c42e4e8f0df05ac29
C.W. Betts <computers57@hotmail.com>
2012-12-10 19:45:55 +0000

    Setting the user default key strings to const.
    git-svn-id: svn://svn.code.sf.net/p/playerpro/code/branches/PlayerPRO6@443 f4605116-7794-4d0b-9e48-65ca9bfeedfd

--- Classes/UserDefaultKeys.h
+++ Classes/UserDefaultKeys.h
@@ -7,58 +7,58 @@
 //
 #import <Foundation/NSString.h>
 
-extern NSString *PPListPreferencesDidChange;
-extern NSString *PPSoundPreferencesDidChange;
-extern NSString *PPDigitalEditorPrefrencesDidChange;
-extern NSString *PPBoxEditorPreferencesDidChange;
-extern NSString *PPMiscPreferencesDidChange;
+extern NSString * const PPListPreferencesDidChange;
+extern NSString * const PPSoundPreferencesDidChange;
+extern NSString * const PPDigitalEditorPrefrencesDidChange;
+extern NSString * const PPBoxEditorPreferencesDidChange;
+extern NSString * const PPMiscPreferencesDidChange;
 
-extern NSString *PPRememberMusicList;
-extern NSString *PPLoadMusicAtListLoad;
-extern NSString *PPAfterPlayingMusic;
-extern NSString *PPGotoStartupAfterPlaying;
-extern NSString *PPSaveModList;
-extern NSString *PPLoadMusicAtMusicLoad;
+extern NSString * const PPRememberMusicList;
+extern NSString * const PPLoadMusicAtListLoad;
+extern NSString * const PPAfterPlayingMusic;
+extern NSString * const PPGotoStartupAfterPlaying;
+extern NSString * const PPSaveModList;
+extern NSString * const PPLoadMusicAtMusicLoad;
 
-extern NSString *PPSoundOutBits;
-extern NSString *PPSoundOutRate;
-extern NSString *PPSoundDriver;
-extern NSString *PPStereoDelayToggle;
-extern NSString *PPReverbToggle;
-extern NSString *PPSurroundToggle;
-extern NSString *PPOversamplingToggle;
-extern NSString *PPStereoDelayAmount;
-extern NSString *PPReverbSize;
-extern NSString *PPReverbStrength;
-extern NSString *PPOversamplingAmount;
+extern NSString * const PPSoundOutBits;
+extern NSString * const PPSoundOutRate;
+extern NSString * const PPSoundDriver;
+extern NSString * const PPStereoDelayToggle;
+extern NSString * const PPReverbToggle;
+extern NSString * const PPSurroundToggle;
+extern NSString * const PPOversamplingToggle;
+extern NSString * const PPStereoDelayAmount;
+extern NSString * const PPReverbSize;
+extern NSString * const PPReverbStrength;
+extern NSString * const PPOversamplingAmount;
 
-extern NSString *PPDEShowInstrument;
-extern NSString *PPDEShowNote;
-extern NSString *PPDEShowEffect;
-extern NSString *PPDEShowArgument;
-extern NSString *PPDEShowVolume;
-extern NSString *PPDEShowMarkers;
-extern NSString *PPDEMarkerOffsetPref;
-extern NSString *PPDEMarkerLoopPref;
-extern NSString *PPDEMarkerColorPref;
-extern NSString *PPDEMouseClickControlPref;
-extern NSString *PPDEMouseClickShiftPref;
-extern NSString *PPDEMouseClickCommandPref;
-extern NSString *PPDEMouseClickOptionPref;
-extern NSString *PPDELineHeightNormal;
-extern NSString *PPDEMusicTraceOn;
-extern NSString *PPDEPatternWrappingPartition;
-extern NSString *PPDEDragAsPcmd;
+extern NSString * const PPDEShowInstrument;
+extern NSString * const PPDEShowNote;
+extern NSString * const PPDEShowEffect;
+extern NSString * const PPDEShowArgument;
+extern NSString * const PPDEShowVolume;
+extern NSString * const PPDEShowMarkers;
+extern NSString * const PPDEMarkerOffsetPref;
+extern NSString * const PPDEMarkerLoopPref;
+extern NSString * const PPDEMarkerColorPref;
+extern NSString * const PPDEMouseClickControlPref;
+extern NSString * const PPDEMouseClickShiftPref;
+extern NSString * const PPDEMouseClickCommandPref;
+extern NSString * const PPDEMouseClickOptionPref;
+extern NSString * const PPDELineHeightNormal;
+extern NSString * const PPDEMusicTraceOn;
+extern NSString * const PPDEPatternWrappingPartition;
+extern NSString * const PPDEDragAsPcmd;
 
-extern NSString *PPBEMarkersEnabled;
-extern NSString *PPBEMarkersOffset;
-extern NSString *PPBEMarkersLoop;
-extern NSString *PPBEOctaveMarkers;
-extern NSString *PPBENotesProjection;
+extern NSString * const PPBEMarkersEnabled;
+extern NSString * const PPBEMarkersOffset;
+extern NSString * const PPBEMarkersLoop;
+extern NSString * const PPBEOctaveMarkers;
+extern NSString * const PPBENotesProjection;
 
-extern NSString *PPMAddExtension;
-extern NSString *PPMMadCompression;
-extern NSString *PPMNoLoadMixerFromFiles;
-extern NSString *PPMOscilloscopeDrawLines;
+extern NSString * const PPMAddExtension;
+extern NSString * const PPMMadCompression;
+extern NSString * const PPMNoLoadMixerFromFiles;
+extern NSString * const PPMOscilloscopeDrawLines;
 
-extern NSString *PPMMusicList;
+extern NSString * const PPMMusicList;
--- Classes/UserDefaultKeys.m
+++ Classes/UserDefaultKeys.m
@@ -8,58 +8,58 @@
 
 #import "UserDefaultKeys.h"
 
-NSString *PPListPreferencesDidChange = @"Music List Preferences Changed";
-NSString *PPSoundPreferencesDidChange = @"Sound Preferences changed";
-NSString *PPDigitalEditorPrefrencesDidChange = @"Digital Editor Preferences changed";
-NSString *PPBoxEditorPreferencesDidChange = @"Box Editor Preferences changed";
-NSString *PPMiscPreferencesDidChange = @"Misc. Preferences Changed";
+NSString * const PPListPreferencesDidChange = @"Music List Preferences Changed";
+NSString * const PPSoundPreferencesDidChange = @"Sound Preferences changed";
+NSString * const PPDigitalEditorPrefrencesDidChange = @"Digital Editor Preferences changed";
+NSString * const PPBoxEditorPreferencesDidChange = @"Box Editor Preferences changed";
+NSString * const PPMiscPreferencesDidChange = @"Misc. Preferences Changed";
 
-NSString *PPRememberMusicList = @"Remember Music List";
-NSString *PPLoadMusicAtListLoad = @"Load music when loading list";
-NSString *PPAfterPlayingMusic = @"After playing music";
-NSString *PPGotoStartupAfterPlaying = @"Go to startup pos. after playing";
-NSString *PPSaveModList = @"Ask to save modified Mod list";
-NSString *PPLoadMusicAtMusicLoad = @"Play music on Music load";
+NSString * const PPRememberMusicList = @"Remember Music List";
+NSString * const PPLoadMusicAtListLoad = @"Load music when loading list";
+NSString * const PPAfterPlayingMusic = @"After playing music";
+NSString * const PPGotoStartupAfterPlaying = @"Go to startup pos. after playing";
+NSString * const PPSaveModList = @"Ask to save modified Mod list";
+NSString * const PPLoadMusicAtMusicLoad = @"Play music on Music load";
 
-NSString *PPSoundOutBits = @"Sound output bits";
-NSString *PPSoundOutRate = @"Sound output rate";
-NSString *PPSoundDriver = @"Sound Driver";
-NSString *PPStereoDelayToggle = @"Stereo delay?";
-NSString *PPReverbToggle = @"Reverb?";
-NSString *PPSurroundToggle = @"Surround?";
-NSString *PPOversamplingToggle = @"Oversampling?";
-NSString *PPStereoDelayAmount = @"Stereo Delay amount";
-NSString *PPReverbSize = @"Reverb Size";
-NSString *PPReverbStrength = @"Reverb Strength";
-NSString *PPOversamplingAmount = @"Oversampling Amount";
+NSString * const PPSoundOutBits = @"Sound output bits";
+NSString * const PPSoundOutRate = @"Sound output rate";
+NSString * const PPSoundDriver = @"Sound Driver";
+NSString * const PPStereoDelayToggle = @"Stereo delay?";
+NSString * const PPReverbToggle = @"Reverb?";
+NSString * const PPSurroundToggle = @"Surround?";
+NSString * const PPOversamplingToggle = @"Oversampling?";
+NSString * const PPStereoDelayAmount = @"Stereo Delay amount";
+NSString * const PPReverbSize = @"Reverb Size";
+NSString * const PPReverbStrength = @"Reverb Strength";
+NSString * const PPOversamplingAmount = @"Oversampling Amount";
 
-NSString *PPDEShowInstrument = @"Digital Editor Show Instrument";
-NSString *PPDEShowNote = @"Digital Editor Show Note";
-NSString *PPDEShowEffect = @"Digital Editor Show Effect";
-NSString *PPDEShowArgument = @"Digital Editor Show Argument";
-NSString *PPDEShowVolume = @"Digital Editor Show Volume";
-NSString *PPDEShowMarkers = @"Digital Editor Show Markers";
-NSString *PPDEMarkerOffsetPref = @"Digital Editor Marker Offset";
-NSString *PPDEMarkerLoopPref = @"Digital Editor Marker Loop";
-NSString *PPDEMarkerColorPref = @"Digital Editor Marker Color";
-NSString *PPDEMouseClickControlPref = @"Digital Editor Mouse Click+Control";
-NSString *PPDEMouseClickShiftPref = @"Digital Editor Mouse Click+Shift";
-NSString *PPDEMouseClickCommandPref = @"Digital Editor Mouse Click+Command";
-NSString *PPDEMouseClickOptionPref = @"Digital EditorMouse Click+Option";
-NSString *PPDELineHeightNormal = @"Digital Editor Line Height Normal?";
-NSString *PPDEMusicTraceOn = @"Digital Editor Trace On?";
-NSString *PPDEPatternWrappingPartition = @"Digital Editor Pattern Wrapping Partition?";
-NSString *PPDEDragAsPcmd = @"Digital Editor Drag as Pcmd?";
+NSString * const PPDEShowInstrument = @"Digital Editor Show Instrument";
+NSString * const PPDEShowNote = @"Digital Editor Show Note";
+NSString * const PPDEShowEffect = @"Digital Editor Show Effect";
+NSString * const PPDEShowArgument = @"Digital Editor Show Argument";
+NSString * const PPDEShowVolume = @"Digital Editor Show Volume";
+NSString * const PPDEShowMarkers = @"Digital Editor Show Markers";
+NSString * const PPDEMarkerOffsetPref = @"Digital Editor Marker Offset";
+NSString * const PPDEMarkerLoopPref = @"Digital Editor Marker Loop";
+NSString * const PPDEMarkerColorPref = @"Digital Editor Marker Color";
+NSString * const PPDEMouseClickControlPref = @"Digital Editor Mouse Click+Control";
+NSString * const PPDEMouseClickShiftPref = @"Digital Editor Mouse Click+Shift";
+NSString * const PPDEMouseClickCommandPref = @"Digital Editor Mouse Click+Command";
+NSString * const PPDEMouseClickOptionPref = @"Digital EditorMouse Click+Option";
+NSString * const PPDELineHeightNormal = @"Digital Editor Line Height Normal?";
+NSString * const PPDEMusicTraceOn = @"Digital Editor Trace On?";
+NSString * const PPDEPatternWrappingPartition = @"Digital Editor Pattern Wrapping Partition?";
+NSString * const PPDEDragAsPcmd = @"Digital Editor Drag as Pcmd?";
 
-NSString *PPBEMarkersEnabled = @"Box Editor Markers On?";
-NSString *PPBEMarkersOffset = @"Box Editor Markers Offset";
-NSString *PPBEMarkersLoop = @"Box Editor Markers Loop";
-NSString *PPBEOctaveMarkers = @"Box Editor Octave Markers?";
-NSString *PPBENotesProjection = @"Box Editor Notes Projection?";
+NSString * const PPBEMarkersEnabled = @"Box Editor Markers On?";
+NSString * const PPBEMarkersOffset = @"Box Editor Markers Offset";
+NSString * const PPBEMarkersLoop = @"Box Editor Markers Loop";
+NSString * const PPBEOctaveMarkers = @"Box Editor Octave Markers?";
+NSString * const PPBENotesProjection = @"Box Editor Notes Projection?";
 
-NSString *PPMAddExtension = @"Add Extension";
-NSString *PPMMadCompression = @"Compress MAD?";
-NSString *PPMNoLoadMixerFromFiles = @"No load Mixer?";
-NSString *PPMOscilloscopeDrawLines = @"Oscilloscope Draw Lines";
+NSString * const PPMAddExtension = @"Add Extension";
+NSString * const PPMMadCompression = @"Compress MAD?";
+NSString * const PPMNoLoadMixerFromFiles = @"No load Mixer?";
+NSString * const PPMOscilloscopeDrawLines = @"Oscilloscope Draw Lines";
 
-NSString *PPMMusicList = @"PlayerPRO Music List";
+NSString * const PPMMusicList = @"PlayerPRO Music List";
