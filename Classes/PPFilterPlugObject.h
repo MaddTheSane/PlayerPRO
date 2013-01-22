b30e420e14a4e1db9bcaa17679fd245e91dd81a8
C.W. Betts <computers57@hotmail.com>
2013-01-22 19:19:04 +0000

    Making the filter plug-in object for future use. Making the backwards filter plug-in build for Cocoa use.
    git-svn-id: svn://svn.code.sf.net/p/playerpro/code/branches/PlayerPRO6@668 f4605116-7794-4d0b-9e48-65ca9bfeedfd

---
+++ Classes/PPFilterPlugObject.h
@@ -0,0 +1,31 @@
+//
+//  PPFilterPlugObject.h
+//  PPMacho
+//
+//  Created by C.W. Betts on 1/22/13.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PPFilterPlugObject : NSObject
+{
+	PPFiltersPlugin	**plugData;
+	NSString		*menuName;
+	NSString		*authorString;
+	NSBundle		*file;
+	OSType			type;
+	UInt32			version;
+}
+
+@property (readonly) NSString *menuName;
+@property (readonly) NSString *authorString;
+@property (readonly) NSBundle *file;
+@property (readonly) OSType type;
+@property (readonly) UInt32 version;
+
+- (id)initWithBundle:(NSBundle *)aBund;
+
+- (OSErr)callPluginWithData:(sData *)theData selectionStart:(long) SelectionStart selectionEnd:(long) SelectionEnd plugInInfo:(PPInfoPlug *)thePPInfoPlug stereoMode:(short)stereoMode;
+
+@end
---
+++ Classes/PPFilterPlugObject.m
@@ -0,0 +1,94 @@
+//
+//  PPFilterPlugObject.m
+//  PPMacho
+//
+//  Created by C.W. Betts on 1/22/13.
+//
+//
+
+#import "PPFilterPlugObject.h"
+#import "PPPlugInCommon.h"
+
+#define PPFilterLoadPlug(theBundle) (PPFiltersPlugin**)GetCOMPlugInterface(theBundle, kPlayerPROFiltersPlugTypeID, kPlayerPROFiltersPlugInterfaceID)
+
+@implementation PPFilterPlugObject
+
+@synthesize authorString;
+@synthesize file;
+@synthesize menuName;
+@synthesize type;
+@synthesize version;
+
+- (id)initWithBundle:(NSBundle *)aBund
+{
+	if (self = [super init]) {
+		{
+			NSURL *bundleURL = [aBund bundleURL];
+			CFBundleRef cfBundle = CFBundleCreate(kCFAllocatorDefault, BRIDGE(CFURLRef, bundleURL));
+			
+			plugData = PPFilterLoadPlug(cfBundle);
+			if (!plugData) {
+				CFRelease(cfBundle);
+				AUTORELEASEOBJNORETURN(self);
+				return nil;
+			}
+			version = CFBundleGetVersionNumber(cfBundle);
+			CFRelease(cfBundle);
+		}
+		
+		NSMutableDictionary *tempDict = [[aBund infoDictionary] mutableCopy];
+		[tempDict addEntriesFromDictionary:[aBund localizedInfoDictionary]];
+		id DictionaryTemp = [tempDict valueForKey:BRIDGE(NSString*, kMadPlugMenuNameKey)];
+		if ([DictionaryTemp isKindOfClass:[NSString class]]) {
+			menuName = [[NSString alloc] initWithString:DictionaryTemp];
+		} else {
+			RELEASEOBJ(tempDict);
+			AUTORELEASEOBJNORETURN(self);
+			return nil;
+		}
+		DictionaryTemp = [tempDict valueForKey:BRIDGE(NSString*, kMadPlugAuthorNameKey)];
+		if (DictionaryTemp) {
+			if ([DictionaryTemp isKindOfClass:[NSString class]]) {
+				authorString = [[NSString alloc] initWithString:DictionaryTemp];
+			} else {
+				authorString = [NSLocalizedString(@"No Author", @"no author") copy];
+			}
+		} else {
+			authorString = [NSLocalizedString(@"No Author", @"no author") copy];
+		}
+		
+		type = 'PLug';
+		
+		RELEASEOBJ(tempDict);
+		
+		file = RETAINOBJ(aBund);
+
+	}
+	return self;
+}
+
+- (OSErr)callPluginWithData:(sData *)theData selectionStart:(long) SelectionStart selectionEnd:(long) SelectionEnd plugInInfo:(PPInfoPlug *)thePPInfoPlug stereoMode:(short)stereoMode
+{
+	CFBundleRef tempBund = CFBundleCreate(kCFAllocatorDefault, BRIDGE(CFURLRef,[file bundleURL]));
+	CFBundleRefNum refNum = CFBundleOpenBundleResourceMap(tempBund);
+	
+	OSErr iErr = (*plugData)->FiltersMain(plugData,theData,SelectionStart, SelectionEnd, thePPInfoPlug, stereoMode);
+	
+	CFBundleCloseBundleResourceMap(tempBund, refNum);
+	CFRelease(tempBund);
+	return iErr;
+}
+
+- (void)dealloc
+{
+	RELEASEOBJ(author);
+	RELEASEOBJ(file);
+	RELEASEOBJ(menuName);
+	if (plugData) {
+		(*plugData)->Release(plugData);
+	}
+	
+	SUPERDEALLOC;
+}
+
+@end
