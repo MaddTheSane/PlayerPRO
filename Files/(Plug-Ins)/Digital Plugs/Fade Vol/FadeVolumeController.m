11f27c096ba63b92d56190c2afe432186e7f677a
C.W. Betts <computers57@hotmail.com>
2013-09-10 17:24:36 -0600

    Begin work on porting the digital plug-ins to Cocoa.

---
+++ Files/(Plug-Ins)/Digital Plugs/Fade Vol/FadeVolumeController.m
@@ -0,0 +1,34 @@
+//
+//  FadeVolumeController.m
+//  PPMacho
+//
+//  Created by C.W. Betts on 9/10/13.
+//
+//
+
+#import "FadeVolumeController.h"
+
+@interface FadeVolumeController ()
+
+@end
+
+@implementation FadeVolumeController
+
+- (id)initWithWindow:(NSWindow *)window
+{
+    self = [super initWithWindow:window];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+- (void)windowDidLoad
+{
+    [super windowDidLoad];
+    
+    // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
+}
+
+@end
---
+++ Files/(Plug-Ins)/Digital Plugs/Note Translate/NoteTranslateController.h
@@ -0,0 +1,13 @@
+//
+//  NoteTranslateController.h
+//  PPMacho
+//
+//  Created by C.W. Betts on 9/10/13.
+//
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface NoteTranslateController : NSWindowController
+
+@end
