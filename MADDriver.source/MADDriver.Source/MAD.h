b211e4d26621714cc712575ac8a62195754ddacd
C.W. Betts <computers57@hotmail.com>
2013-09-10 15:08:43 -0600

    Make some unicode datatypes.

--- MADDriver.source/MADDriver.Source/MAD.h
+++ MADDriver.source/MADDriver.Source/MAD.h
@@ -140,7 +140,8 @@ enum {
 #define MAXPOINTER			999
 #define MAXTRACK			256
 //#define MAXTRACKMULTI		99
-#define MADID				'MADK'
+#define MADIDMAC			'MADK'
+#define MADID				'MADu'
 
 // ***	
 // ***	PATTERN DESCRIPTION
@@ -167,12 +168,28 @@ typedef struct PatHeader					// HEADER
 } PatHeader;
 typedef PatHeader PatternHeader;
 
+typedef struct PatternHeaderUnicode					// HEADER
+{
+	SInt32	size;					// Length of pattern: standard = 64
+	OSType	compMode;				// Compression mode, none = 'NONE'
+	wchar_t	name[ 32];
+	SInt32	patBytes;				// Pattern Size in Bytes
+	SInt32	unused2;
+} PatternHeaderUnicode;
+
 typedef struct PatData						// DATA STRUCTURE : HEADER + COMMANDS
 {									// Pattern = 64 notes to play
 	PatHeader	header;
 	Cmd			Cmds[ 1];
 } PatData;
 typedef PatData PatternData;
+
+
+typedef struct PatternDataUnicode						// DATA STRUCTURE : HEADER + COMMANDS
+{									// Pattern = 64 notes to play
+	PatternHeaderUnicode	header;
+	Cmd						Cmds[ 1];
+} PatternDataUnicode;
 
 
 // ***	
@@ -213,6 +230,38 @@ typedef struct sData32								// SAMPLE
 	UInt32				data;				// Used only in memory, not in files
 } sData32;
 typedef sData32 SampleData32;
+
+typedef union SampleDataUnicode								// SAMPLE
+{
+	struct {
+		SInt32 				size;				// Sample length
+		SInt32				loopBeg;			// LoopStart
+		SInt32				loopSize;			// LoopLength
+		Byte 				vol;				// Base volume
+		unsigned short		c2spd;				// c2spd
+		Byte				loopType;
+		Byte				amp;				// 8 or 16 bits
+		char				relNote;
+		wchar_t				name[32];			// Sample name
+		Byte				stereo;				// Stereo
+		Ptr					data;				// Used only in memory, not in files
+	} SampleDataUnicodeMem;
+	struct {
+		SInt32 				size;				// Sample length
+		SInt32				loopBeg;			// LoopStart
+		SInt32				loopSize;			// LoopLength
+		Byte 				vol;				// Base volume
+		unsigned short		c2spd;				// c2spd
+		Byte				loopType;
+		Byte				amp;				// 8 or 16 bits
+		char				relNote;
+		wchar_t				name[32];			// Sample name
+		Byte				stereo;				// Stereo
+		UInt32				data;				// Used only in memory, not in files
+	} SampleDataUnicodeFile;
+
+} SampleDataUnicode;
+
 
 enum
 {
@@ -273,6 +322,50 @@ typedef struct InstrData				// INSTRUMENT
 } InstrData;
 typedef InstrData InstrumentData;
 
+typedef struct InstrumentDataUnicode				// INSTRUMENT
+{
+	wchar_t	name[ 32];			// instrument name
+	Byte 	type;				// Instrument type = 0
+	Byte	no;					// Instrument number
+	
+	short	firstSample;		// First sample ID in sample list
+	short	numSamples;			// Number of samples in instrument
+	
+	short	MIDI;				// MIDI Channel Value
+	
+	short	MIDIType;			// 0 Sound output only, 1 MIDI output only, 2 both
+	
+	/**/
+	
+	Byte	what[ 96];			// Sample number for all notes
+	EnvRec 	volEnv[ 12];		// Points for volume envelope
+	EnvRec	pannEnv[ 12];		// Points for panning envelope
+	EnvRec	pitchEnv[ 12];		// Points for panning envelope
+	
+	Byte	volSize;			// Number of volume points
+	Byte	pannSize;			// Number of panning points
+	Byte	pitchSize;			// Number of panning points
+	
+	Byte	volSus;				// Volume sustain point
+	Byte	volBeg;				// Volume loop start point
+	Byte	volEnd;				// Volume loop end point
+	
+	Byte	pannSus;			// Panning sustain point
+	Byte	pannBeg;			// Panning loop start point
+	Byte	pannEnd;			// Panning loop end point
+	
+	Byte	pitchSus;			// Pitch sustain point
+	Byte	pitchBeg;			// Pitch loop start point
+	Byte	pitchEnd;			// Pitch loop end point
+	
+	Byte	volType;			// Volume type: bit 0: On; 1: Sustain; 2: Loop
+	Byte	pannType;			// Panning type: bit 0: On; 1: Sustain; 2: Loop
+	
+	unsigned short	volFade;	// Volume fadeout
+	
+	Byte	vibDepth;
+	Byte	vibRate;
+} InstrumentDataUnicode;
 
 enum
 {
@@ -291,13 +384,13 @@ enum
 #define INFOSSIZE 239
 
 
-typedef struct
+typedef struct FXBus
 {
 	Boolean		ByPass;
 	short		copyId;
 	Boolean		Active;
 	
-}	FXBus;
+} FXBus;
 
 typedef struct MADSpec
 {
@@ -333,6 +426,41 @@ typedef struct MADSpec
 	FXBus		chanBus[ MAXTRACK];
 } MADSpec;
 
+typedef struct MADSpecUni
+{
+	OSType		MAD;						// Mad Identification
+	wchar_t		name[ 32];					// Music's name
+	wchar_t		infos[ INFOSSIZE];			// Informations & Author name of the music
+	Byte		generalPan;					// General Panning
+	Byte		MultiChanNo;				// Number of chan for multichannel
+	Byte		MultiChan;					// MultiChannel per tracks?
+	SInt32		EPitch;						// New Pitch
+	SInt32		ESpeed;						// New Speed
+	Byte		XMLinear;					// Linear picth table?
+	Byte		MODMode;					// Limit pitch to MOD pitch table
+	Byte		showCopyright;				// Show infos at startup? true or false
+	Byte		generalPitch;				// General Pitch
+	Byte		generalSpeed;				// General Speed
+	Byte		generalVol;					// Software general volume
+	Byte		numPat;						// Patterns number
+	Byte		numChn;						// Channels number
+	Byte 		numPointers;				// Partition length
+	Byte		numInstru;					// Instruments number
+	Byte		numSamples;					// Samples number
+	Byte		oPointers[ MAXPOINTER];		// Partition : Patterns ID List
+	short		speed;						// Default speed
+	short		tempo;						// Default tempo
+	Byte		chanPan[ MAXTRACK];			// Channel settings, from 0 to 256
+	Byte		chanVol[ MAXTRACK];			// Channel Volume, from 0 to 64
+	
+	SInt32		globalEffect[ 10];			// Global Effects IDs
+	Boolean		globalFXActive;				// Global FX Active?
+	
+	SInt32		chanEffect[ MAXTRACK][ 4];	// Channel Effect IDs
+	FXBus		chanBus[ MAXTRACK];
+} MADSpecUni;
+
+
 typedef struct FXSets
 {
 	short	track;
@@ -343,6 +471,15 @@ typedef struct FXSets
 	Str63	name;
 } FXSets;	// and then float values
 
+typedef struct FXSetsUnicode
+{
+	short	track;
+	short	id;
+	SInt32	FXID;
+	short	noArg;
+	float	values[ 100];
+	wchar_t	name[64];
+} FXSetsUnicode;	// and then float values
 
 #pragma pack(pop)
 
--- MADDriver.source/MADDriver.Source/PPDefs.h
+++ MADDriver.source/MADDriver.Source/PPDefs.h
@@ -24,6 +24,8 @@
 #endif
 #endif
 
+#include <wchar.h>
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
