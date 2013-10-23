5e6e44ce71fcab0b6e8588e9b6aa2aeae38558d5
C.W. Betts <computers57@hotmail.com>
2013-10-23 11:29:47 -0600

    Since RubyCocoa was removed, using an Objective-C tool to do the same thing.

---
+++ updateInfoPlist/main.m
@@ -0,0 +1,64 @@
+//
+//  main.m
+//  updateInfoPlist
+//
+//  Created by C.W. Betts on 10/23/13.
+//  Code based on the RubyCocoa script used by the MPlayerX team
+//  Original code can be found at http://blog.mplayerx.org/blog/2013/05/10/use-version-number-with-xcode-and-git/
+//
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+	@autoreleasepool {
+		if (argc != 3) {
+			fprintf(stderr, "Usage: %s \"path to plist\" kindOfApplication", argv[0]);
+			abort();
+			return EXIT_FAILURE;
+		}
+		NSString *plistLocation =[[NSFileManager defaultManager] stringWithFileSystemRepresentation:argv[1] length:strlen(argv[1])];
+	    NSMutableDictionary *plistDict = [[NSMutableDictionary alloc] initWithContentsOfFile:plistLocation];
+		if (!plistDict) {
+			fprintf(stderr, "Could not open dictionary!\n");
+			return EXIT_FAILURE;
+		}
+		NSTask *versionTask = [[NSTask alloc] init];
+		[versionTask setLaunchPath:@"/bin/bash"];
+		versionTask.arguments = @[@"version.sh"];
+		NSPipe *soPipe = [NSPipe new];
+		versionTask.standardOutput = soPipe;
+		
+		[versionTask launch];
+		[versionTask waitUntilExit];
+		
+		NSData *outData = [soPipe.fileHandleForReading readDataToEndOfFile];
+		versionTask = nil;
+		NSString *outString = [[NSString alloc] initWithData:outData encoding:NSUTF8StringEncoding];
+		outString = [outString stringByTrimmingCharactersInSet:[NSCharacterSet newlineCharacterSet]];
+		if ([outString isEqualToString:@"unknown"]) {
+			fprintf(stderr, "Version is invalid!\n");
+			return EXIT_FAILURE;
+		}
+		if (strcasecmp(argv[2], "player") == 0) {
+			plistDict[@"CFBundleVersion"] = outString;
+		} else {
+			plistDict[@"CFBundleVersion"] = [NSString stringWithFormat:@"6.0.0.%@", outString];
+		}
+		NSTask *hashTask = [[NSTask alloc] init];
+		hashTask.launchPath = @"/usr/bin/git";
+		hashTask.arguments = @[@"rev-list", @"--max-count=1", @"HEAD"];
+		hashTask.standardOutput = soPipe = [NSPipe new];
+		
+		[hashTask launch];
+		[hashTask waitUntilExit];
+		
+		NSData *commitHash = [soPipe.fileHandleForReading readDataToEndOfFile];
+		outString = [[[NSString alloc] initWithData:commitHash encoding:NSUTF8StringEncoding] stringByTrimmingCharactersInSet:[NSCharacterSet newlineCharacterSet]];
+		plistDict[@"MPXCommitHash"] = outString;
+		[plistDict writeToFile:[NSString stringWithFormat:@"%@vers.plist", plistLocation] atomically:NO];
+	}
+    return 0;
+}
+
