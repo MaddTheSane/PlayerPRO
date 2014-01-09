53556a931a0527f06a6d14faf11553cfc6ffb6bc
C.W. Betts <computers57@hotmail.com>
2014-01-08 19:14:20 -0700

    Re-write the mins instrument import-export plug-in to use CF*Streams instead of using PlayerPROCore’s file functions.

--- Files/(Plug-Ins)/Instruments Import-Export Plugs/MINS.c
+++ Files/(Plug-Ins)/Instruments Import-Export Plugs/MINS.c
@@ -37,52 +37,6 @@ static OSErr MAD2KillInstrument(InstrData *curIns, sData **sample)
 	bzero(curIns, sizeof(InstrData));
 	curIns->firstSample = firstSample;
 	
-#if 0
-	for (i = 0; i < 32; i++) curIns->name[i]	= 0;
-	curIns->type		= 0;
-	curIns->numSamples	= 0;
-	
-	/**/
-	
-#if 1
-	memset(curIns->what, 0, sizeof(curIns->what));
-	memset(curIns->volEnv, 0, sizeof(curIns->volEnv));
-	memset(curIns->pannEnv, 0, sizeof(curIns->pannEnv));
-	memset(curIns->pitchEnv, 0, sizeof(curIns->pitchEnv));
-#else
-	for (i = 0; i < 96; i++) curIns->what[ i]		= 0;
-	
-	for (i = 0; i < 12; i++)
-	{
-		curIns->volEnv[ i].pos		= 0;
-		curIns->volEnv[ i].val		= 0;
-		
-		curIns->pannEnv[ i].pos	= 0;
-		curIns->pannEnv[ i].val	= 0;
-		
-		curIns->pitchEnv[ i].pos	= 0;
-		curIns->pitchEnv[ i].val	= 0;
-		
-	}
-#endif
-	curIns->volSize		= 0;
-	curIns->pannSize	= 0;
-	
-	curIns->volSus		= 0;
-	curIns->volBeg		= 0;
-	curIns->volEnd		= 0;
-	
-	curIns->pannSus		= 0;
-	curIns->pannBeg		= 0;
-	curIns->pannEnd		= 0;
-	
-	curIns->volType		= 0;
-	curIns->pannType	= 0;
-	
-	curIns->volFade		= 0;
-	curIns->vibDepth	= 0;
-	curIns->vibRate		= 0;
-#endif
 	return noErr;
 }
 
@@ -105,152 +59,127 @@ static inline void ByteswapInstrument(InstrData *toswap)
 	dispatch_apply(12, dispatch_get_global_queue(0, 0), ^(size_t i) {
 		PPBE16(&toswap->pannEnv[i].pos);
 		PPBE16(&toswap->pannEnv[i].val);
-		
 		PPBE16(&toswap->pitchEnv[i].pos);
 		PPBE16(&toswap->pitchEnv[i].val);
-		
 		PPBE16(&toswap->volEnv[i].pos);
 		PPBE16(&toswap->volEnv[i].val);
 	});
 }
 
-//hack around the fact that there isn't an equivalent of CFStringGetMaximumSizeOfFileSystemRepresentation for CFURLs
-static CFIndex getCFURLFilePathRepresentationLength(CFURLRef theRef, Boolean resolveAgainstBase)
+static OSErr mainMINs(void			*unused,
+					  OSType		order,						// Order to execute
+					  InstrData		*InsHeader,					// Ptr on instrument header
+					  sData			**sample,					// Ptr on samples data
+					  short			*sampleID,					// If you need to replace/add only a sample, not replace the entire instrument (by example for 'AIFF' sound)
+					  // If sampleID == -1 : add sample else replace selected sample.
+					  CFURLRef		AlienFileCFURL,				// IN/OUT file
+					  PPInfoPlug	*thePPInfoPlug)
 {
-	CFStringRef fileString = CFURLCopyFileSystemPath(theRef, kCFURLPOSIXPathStyle);
-	CFIndex strLength = CFStringGetMaximumSizeOfFileSystemRepresentation(fileString);
-	CFRelease(fileString);
-	return strLength;
-}
-
-static OSErr mainMINs(void		*unused,
-			   OSType		order,						// Order to execute
-			   InstrData	*InsHeader,					// Ptr on instrument header
-			   sData		**sample,					// Ptr on samples data
-			   short		*sampleID,					// If you need to replace/add only a sample, not replace the entire instrument (by example for 'AIFF' sound)
-			   // If sampleID == -1 : add sample else replace selected sample.
-			   CFURLRef		AlienFileCFURL,				// IN/OUT file
-			   PPInfoPlug	*thePPInfoPlug)
-{
-	OSErr	myErr = noErr;
-	UNFILE	iFileRefI;
-	short	x;
-	long	inOutCount;
-	Ptr		theSound;
+	OSErr				myErr = noErr;
+	short				x;
+	long				inOutCount;
+	UInt8*				theSound;
+	CFStreamStatus		theStatus;
+	CFReadStreamRef		readStream;
+	CFWriteStreamRef	writeStream;
 	
-	char *file = NULL;
-	do {
-		char *longStr = NULL;
-		CFIndex pathLen = getCFURLFilePathRepresentationLength(AlienFileCFURL, TRUE);
-		longStr = malloc(pathLen);
-		if (!longStr) {
-			return MADNeedMemory;
-		}
-		Boolean pathOK = CFURLGetFileSystemRepresentation(AlienFileCFURL, true, (unsigned char*)longStr, pathLen);
-		if (!pathOK) {
-			free(longStr);
-			return MADReadingErr;
-		}
-		size_t StrLen = strlen(longStr);
-		file = realloc(longStr, ++StrLen);
-		if (!file) {
-			file = longStr;
-		}
-	} while (0);
-	
-	switch (order)
-	{
-		case 'IMPL':
-			iFileRefI = iFileOpenRead(file);
-			if (iFileRefI != NULL) {
-				inOutCount = iGetEOF(iFileRefI);
+	switch (order) {
+		case MADPlugImport:
+			readStream = CFReadStreamCreateWithFile(kCFAllocatorDefault, AlienFileCFURL);
+			if (CFReadStreamOpen(readStream)) {
+				theStatus = CFReadStreamGetStatus(readStream);
+				while (theStatus != kCFStreamStatusOpen && theStatus != kCFStreamStatusError) {
+					theStatus = CFReadStreamGetStatus(readStream);
+				}
+				if (theStatus == kCFStreamStatusError) {
+					CFReadStreamClose(readStream);
+					CFRelease(readStream);
+					return MADReadingErr;
+				}
+				MAD2KillInstrument(InsHeader, sample);
 				
-				theSound = malloc(inOutCount);
-				if (theSound == NULL)
-					myErr = MADNeedMemory;
-				else {
-					free(theSound);
+				// READ instrument header
+				
+				inOutCount = sizeof(InstrData);
+				CFReadStreamRead(readStream, (UInt8*)InsHeader, inOutCount);
+				ByteswapInstrument(InsHeader);
+				
+				// READ samples headers & data
+				
+				for (x = 0; x < InsHeader->numSamples; x++) {
+					sData *curData = sample[x] = inMADCreateSample();
 					
-					MAD2KillInstrument(InsHeader, sample);
-					
-					// READ instrument header
-					
-					inOutCount = sizeof(InstrData);
-					
-					iRead(inOutCount, InsHeader, iFileRefI);
-					ByteswapInstrument(InsHeader);
-					
-					// READ samples headers & data
-					
-					for (x = 0; x < InsHeader->numSamples; x++) {
-						sData *curData = sample[x] = inMADCreateSample();
-						
-						inOutCount = sizeof(sData32);
-						
-						myErr = iRead(inOutCount, curData, iFileRefI);
-						
-						ByteswapsData(curData);
-						
-						curData->data = malloc(curData->size);
-						if (curData->data != NULL) {
-							inOutCount = curData->size;
-							myErr = iRead(inOutCount, (Ptr)curData->data, iFileRefI);
-						}
-						
-						if (curData->amp == 16) {
-							short	*shortPtr = (short*)curData->data;
-							
-							dispatch_apply(curData->size / 2, dispatch_get_global_queue(0, 0), ^(size_t ll) {
-								PPBE16(&shortPtr[ll]);
-							});
-						}
-						
+					inOutCount = sizeof(sData32);
+					CFReadStreamRead(readStream, (UInt8*)curData, inOutCount);
+					ByteswapsData(curData);
+					curData->data = malloc(curData->size);
+					if (curData->data != NULL) {
+						inOutCount = curData->size;
+						CFReadStreamRead(readStream, (UInt8*)curData->data, inOutCount);
+					}
+					if (curData->amp == 16) {
+						short	*shortPtr = (short*)curData->data;
+						dispatch_apply(curData->size / 2, dispatch_get_global_queue(0, 0), ^(size_t ll) {
+							PPBE16(&shortPtr[ll]);
+						});
 					}
 				}
 				
-				iClose(iFileRefI);
+				CFReadStreamClose(readStream);
+				CFRelease(readStream);
 			} else
 				myErr = MADReadingErr;
 			
 			break;
 			
-		case 'TEST':
-			iFileRefI = iFileOpenRead(file);
-			if (iFileRefI != NULL) {
+		case MADPlugTest:
+			readStream = CFReadStreamCreateWithFile(kCFAllocatorDefault, AlienFileCFURL);
+			if (CFReadStreamOpen(readStream)) {
+				theStatus = CFReadStreamGetStatus(readStream);
+				while (theStatus != kCFStreamStatusOpen && theStatus != kCFStreamStatusError) {
+					theStatus = CFReadStreamGetStatus(readStream);
+				}
+				if (theStatus == kCFStreamStatusError) {
+					CFReadStreamClose(readStream);
+					CFRelease(readStream);
+					return MADReadingErr;
+				}
 				inOutCount = sizeof(InstrData);
 				theSound = malloc(inOutCount);
-				if (theSound == NULL)
+				if (theSound == NULL) {
+					CFReadStreamClose(readStream);
+					CFRelease(readStream);
 					myErr = MADNeedMemory;
-				else {
-					iRead(inOutCount, theSound, iFileRefI);
-					
+				} else {
+					CFReadStreamRead(readStream, theSound, inOutCount);
 					ByteswapInstrument((InstrData*)theSound);
-					
 					myErr = TestMINS((InstrData*)theSound);
 				}
 				
-				free(theSound);
-				iClose(iFileRefI);
+				CFReadStreamClose(readStream);
+				CFRelease(readStream);
 			} else
 				myErr = MADReadingErr;
 			
 			break;
 			
-		case 'EXPL':
-			
-			iFileCreate(file, 'MINs');
-			iFileRefI = iFileOpenWrite(file);
-			
-			if (iFileRefI != NULL) {
-				// Write instrument header
+		case MADPlugExport:
+			writeStream = CFWriteStreamCreateWithFile(kCFAllocatorDefault, AlienFileCFURL);
+			if (CFWriteStreamOpen(writeStream)) {
+				theStatus = CFWriteStreamGetStatus(writeStream);
+				while (theStatus != kCFStreamStatusOpen && theStatus != kCFStreamStatusError) {
+					theStatus = CFWriteStreamGetStatus(writeStream);
+				}
+				if (theStatus == kCFStreamStatusError) {
+					CFWriteStreamClose(writeStream);
+					CFRelease(writeStream);
+					return MADWritingErr;
+				}
 				inOutCount = sizeof(InstrData);
 				InstrData *tempIns = malloc(inOutCount);
 				memcpy(tempIns, InsHeader, inOutCount);
 				ByteswapInstrument(tempIns);
-				iWrite(inOutCount, tempIns, iFileRefI);
-				free(tempIns);
-				
-				// Write samples headers & data
+				CFWriteStreamWrite(writeStream, (UInt8*)tempIns, inOutCount);
 				for (x = 0; x < InsHeader->numSamples; x++) {
 					sData	*curData;
 					sData	*copyData;
@@ -264,36 +193,33 @@ static OSErr mainMINs(void		*unused,
 					memcpy(copydataData, curData->data, curData->size);
 					if (curData->amp == 16) {
 						short	*shortPtr = (short*) copydataData;
-						
 						dispatch_apply(curData->size / 2, dispatch_get_global_queue(0, 0), ^(size_t ll) {
 							PPBE16(&shortPtr[ll]);
-							
 						});
-						
 					}
 					
 					memcpy(&toWrite, copyData, sizeof(sData32));
 					toWrite.data = 0;
 					
 					inOutCount = sizeof(sData32);
-					iWrite(inOutCount, &toWrite, iFileRefI);
+					CFWriteStreamWrite(writeStream, (UInt8*)&toWrite, inOutCount);
 					
 					inOutCount = curData->size;
-					iWrite(inOutCount, copydataData, iFileRefI);
+					CFWriteStreamWrite(writeStream, (UInt8*)copydataData, inOutCount);
 					free(copyData);
 					free(copydataData);
 				}
-				iClose(iFileRefI);
+				CFWriteStreamClose(writeStream);
+				CFRelease(writeStream);
 			} else
 				myErr = MADWritingErr;
+			
 			break;
 			
 		default:
 			myErr = MADOrderNotImplemented;
 			break;
 	}
-	
-	free(file);
 	
 	return myErr;
 }
