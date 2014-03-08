59b161223d2daf560ea3aaec709a7041297a17f6
C.W. Betts <computers57@hotmail.com>
2014-03-08 10:04:47 -0700

    Better handling of the PPErrors localizable strings.

--- Shared/PPErrors.m
+++ Shared/PPErrors.m
@@ -8,11 +8,28 @@
 
 #import "PPErrors.h"
 #include <PlayerPROCore/PlayerPROCore.h>
+#if defined(NOEXPORTFUNCS) && NOEXPORTFUNCS
+#import "PPMusicObject.h"
+#endif
 
 NSString * const PPMADErrorDomain = @"net.sourceforge.playerpro.PlayerPROKit.ErrorDomain";
 
+#if defined(NOEXPORTFUNCS) && NOEXPORTFUNCS
+#define BUNDLEINIT
+#define PPErrorLocalizedString(theKey, comment) NSLocalizedStringWithDefaultValue(theKey, @"PPErrors", [NSBundle mainBundle], theKey, comment)
+#else
+#define BUNDLEINIT static NSBundle *PPKBundle \
+static dispatch_once_t errorOnceToken; \
+dispatch_once(&errorOnceToken, ^{ \
+PPKBundle = [NSBundle bundleForClass:[PPMusicObject class]]; \
+});
+
+#define PPErrorLocalizedString(theKey, comment) NSLocalizedStringWithDefaultValue(theKey, @"PPErrors", PPKBundle, theKey, comment)
+#endif
+
 NSError *CreateErrorFromMADErrorType(OSErr theErr)
 {
+	BUNDLEINIT;
 	NSString *ErrorDescription;
 	NSString *errorReason;
 	NSString *recoverySuggestion;
@@ -23,87 +40,87 @@ NSError *CreateErrorFromMADErrorType(OSErr theErr)
 			break;
 			
 		case MADNeedMemory:
-			ErrorDescription = NSLocalizedString(@"Not enough memory for operation", @"Not enough memory");
-			errorReason = NSLocalizedString(@"Ran out of memory", @"Ran out of memory");
-			recoverySuggestion = NSLocalizedString(@"Try closing some applications or closing windows.", @"Close apps");
+			ErrorDescription = PPErrorLocalizedString(@"Not enough memory for operation", @"Not enough memory");
+			errorReason = PPErrorLocalizedString(@"Ran out of memory", @"Ran out of memory");
+			recoverySuggestion = PPErrorLocalizedString(@"Try closing some applications or closing windows.", @"Close apps");
 			break;
 			
 		case MADReadingErr:
-			ErrorDescription = NSLocalizedString(@"Error reading file", @"Error reading file");
-			errorReason = NSLocalizedString(@"Could not read file", @"");
-			recoverySuggestion = NSLocalizedString(@"check to see if you have read permissions for the file.", @"Can you read it?");
+			ErrorDescription = PPErrorLocalizedString(@"Error reading file", @"Error reading file");
+			errorReason = PPErrorLocalizedString(@"Could not read file", @"");
+			recoverySuggestion = PPErrorLocalizedString(@"check to see if you have read permissions for the file.", @"Can you read it?");
 			break;
 			
 		case MADIncompatibleFile:
-			ErrorDescription = NSLocalizedString(@"The file is incompatible with PlayerPRO or one of it's plug-ins", @"Incompatible file");
-			errorReason = NSLocalizedString(@"The file is incompatible", @"");
-			recoverySuggestion = NSLocalizedString(@"Check the file to make sure it is a valid file.", @"Check file");
+			ErrorDescription = PPErrorLocalizedString(@"The file is incompatible with PlayerPRO or one of it's plug-ins", @"Incompatible file");
+			errorReason = PPErrorLocalizedString(@"The file is incompatible", @"");
+			recoverySuggestion = PPErrorLocalizedString(@"Check the file to make sure it is a valid file.", @"Check file");
 			break;
 			
 		case MADLibraryNotInitialized:
-			ErrorDescription = NSLocalizedString(@"The library is not initialized", @"");
-			errorReason = NSLocalizedString(@"library not initalized", @"");
-			recoverySuggestion = NSLocalizedString(@"Contact the developer with what you were doing to cause this error.", @"Contact Developer");
+			ErrorDescription = PPErrorLocalizedString(@"The library is not initialized", @"");
+			errorReason = PPErrorLocalizedString(@"library not initalized", @"");
+			recoverySuggestion = PPErrorLocalizedString(@"Contact the developer with what you were doing to cause this error.", @"Contact Developer");
 			break;
 			
 		case MADParametersErr:
-			ErrorDescription = NSLocalizedString(@"Invalid paramaters were sent to a function", @"Invalid parameters");
-			errorReason = NSLocalizedString(@"Invalid parameters", @"Invalid parameters");
-			recoverySuggestion = NSLocalizedString(@"Contact the developer with what you were doing to cause this error.", @"Contact Developer");
+			ErrorDescription = PPErrorLocalizedString(@"Invalid paramaters were sent to a function", @"Invalid parameters");
+			errorReason = PPErrorLocalizedString(@"Invalid parameters", @"Invalid parameters");
+			recoverySuggestion = PPErrorLocalizedString(@"Contact the developer with what you were doing to cause this error.", @"Contact Developer");
 			break;
 			
 		case MADSoundManagerErr:
-			ErrorDescription = NSLocalizedString(@"Error initalizing the sound system", @"Sound system error");
-			errorReason = NSLocalizedString(@"Sound system initialization failure", @"");
-			recoverySuggestion = NSLocalizedString(@"Make sure that the sound settings are supported by the sound system", @"");
+			ErrorDescription = PPErrorLocalizedString(@"Error initalizing the sound system", @"Sound system error");
+			errorReason = PPErrorLocalizedString(@"Sound system initialization failure", @"");
+			recoverySuggestion = PPErrorLocalizedString(@"Make sure that the sound settings are supported by the sound system", @"");
 			break;
 			
 		case MADOrderNotImplemented:
-			ErrorDescription = NSLocalizedString(@"The selected order is not implemented in the plug-in", @"");
-			errorReason = NSLocalizedString(@"order not implemented", @"");
-			recoverySuggestion = NSLocalizedString(@"Contact the developer with what you were doing to cause this error.", @"Contact Developer");
+			ErrorDescription = PPErrorLocalizedString(@"The selected order is not implemented in the plug-in", @"");
+			errorReason = PPErrorLocalizedString(@"order not implemented", @"");
+			recoverySuggestion = PPErrorLocalizedString(@"Contact the developer with what you were doing to cause this error.", @"Contact Developer");
 			break;
 			
 		case MADFileNotSupportedByThisPlug:
-			ErrorDescription = NSLocalizedString(@"The file is not supported by this plug-in", @"");
-			errorReason = NSLocalizedString(@"Invalid file for plug-in", @"");
-			recoverySuggestion = NSLocalizedString(@"Try using another plug-in, or checking to see if the file is okay.", @"try other plug-in");
+			ErrorDescription = PPErrorLocalizedString(@"The file is not supported by this plug-in", @"");
+			errorReason = PPErrorLocalizedString(@"Invalid file for plug-in", @"");
+			recoverySuggestion = PPErrorLocalizedString(@"Try using another plug-in, or checking to see if the file is okay.", @"try other plug-in");
 			break;
 			
 		case MADCannotFindPlug:
-			ErrorDescription = NSLocalizedString(@"Unable to locate a plug-in to play this file", @"unknown plug-in");
-			errorReason = NSLocalizedString(@"Unable to locate a plug-in", @"");
-			recoverySuggestion = NSLocalizedString(@"Make sure that the file is valid. Also try looking for a compatible plug-in.", @"");
+			ErrorDescription = PPErrorLocalizedString(@"Unable to locate a plug-in to play this file", @"unknown plug-in");
+			errorReason = PPErrorLocalizedString(@"Unable to locate a plug-in", @"");
+			recoverySuggestion = PPErrorLocalizedString(@"Make sure that the file is valid. Also try looking for a compatible plug-in.", @"");
 			break;
 			
 		case MADMusicHasNoDriver:
-			ErrorDescription = NSLocalizedString(@"The MAD music has no driver", @"");
-			errorReason = NSLocalizedString(@"Music has no driver", @"");
-			recoverySuggestion = NSLocalizedString(@"Contact the developer with what you were doing to cause this error.", @"Contact Developer");
+			ErrorDescription = PPErrorLocalizedString(@"The MAD music has no driver", @"");
+			errorReason = PPErrorLocalizedString(@"Music has no driver", @"");
+			recoverySuggestion = PPErrorLocalizedString(@"Contact the developer with what you were doing to cause this error.", @"Contact Developer");
 			break;
 			
 		case MADDriverHasNoMusic:
-			ErrorDescription = NSLocalizedString(@"The MAD Driver has no music", @"");
-			errorReason = NSLocalizedString(@"Driver has no music", @"");
-			recoverySuggestion = NSLocalizedString(@"Try attaching music to the driver by selecting the music.", @"");
+			ErrorDescription = PPErrorLocalizedString(@"The MAD Driver has no music", @"");
+			errorReason = PPErrorLocalizedString(@"Driver has no music", @"");
+			recoverySuggestion = PPErrorLocalizedString(@"Try attaching music to the driver by selecting the music.", @"");
 			break;
 			
 		case MADSoundSystemUnavailable:
-			ErrorDescription = NSLocalizedString(@"The selected sound system is not available", @"");
-			errorReason = NSLocalizedString(@"Sound system is unavailable", @"");
-			recoverySuggestion = NSLocalizedString(@"Try selecting a different sound system.", @"");
+			ErrorDescription = PPErrorLocalizedString(@"The selected sound system is not available", @"");
+			errorReason = PPErrorLocalizedString(@"Sound system is unavailable", @"");
+			recoverySuggestion = PPErrorLocalizedString(@"Try selecting a different sound system.", @"");
 			break;
 			
 		case MADWritingErr:
-			ErrorDescription = NSLocalizedString(@"Unable to write to file", @"");
-			errorReason = NSLocalizedString(@"Unable to write", @"");
-			recoverySuggestion = NSLocalizedString(@"Make sure you have write permissions at the location you selected.", @"");
+			ErrorDescription = PPErrorLocalizedString(@"Unable to write to file", @"");
+			errorReason = PPErrorLocalizedString(@"Unable to write", @"");
+			recoverySuggestion = PPErrorLocalizedString(@"Make sure you have write permissions at the location you selected.", @"");
 			break;
 			
 		case MADUnknownErr:
-			ErrorDescription = NSLocalizedString(@"An unknown error occured", @"Unknown error");
-			errorReason = NSLocalizedString(@"Unknown reason", @"unknown reason");
-			recoverySuggestion = NSLocalizedString(@"Contact the developer with what you were doing to cause this error.", @"Contact Developer");
+			ErrorDescription = PPErrorLocalizedString(@"An unknown error occured", @"Unknown error");
+			errorReason = PPErrorLocalizedString(@"Unknown reason", @"unknown reason");
+			recoverySuggestion = PPErrorLocalizedString(@"Contact the developer with what you were doing to cause this error.", @"Contact Developer");
 			break;
 			
 		default:
