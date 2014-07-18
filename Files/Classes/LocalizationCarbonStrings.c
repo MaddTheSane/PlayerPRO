cb3def82607a129ce00f7b325e17bee34a9e295e
C.W. Betts <computers57@hotmail.com>
2014-07-17 23:02:55 -0600

    Remove commented-out code. The localization source code don't need to be an Objective C source file.

---
+++ Files/Classes/LocalizationCarbonStrings.c
@@ -0,0 +1,22 @@
+/*
+ *  LocalizationCarbonStrings.c
+ *  PPMacho
+ *
+ *  Created by C.W. Betts on 7/17/14.
+ *  Copyright 2014 __MyCompanyName__. All rights reserved.
+ *
+ */
+
+#include "LocalizationCarbonStrings.h"
+#include <CoreFoundation/CoreFoundation.h>
+
+void GetCFIndexString(Str255 outStr, short listID, short indexID)
+{
+	CFStringRef strLocation = CFStringCreateWithFormat(kCFAllocatorDefault, NULL, CFSTR("%hd,%hd"), listID, indexID - 1);
+	CFStringRef localizedStr = CFBundleCopyLocalizedString(CFBundleGetMainBundle(), strLocation, NULL, NULL);
+	CFStringGetPascalString(localizedStr, outStr, sizeof(Str255), kCFStringEncodingMacRoman);
+	
+	
+	CFRelease(strLocation);
+	CFRelease(localizedStr);
+}
