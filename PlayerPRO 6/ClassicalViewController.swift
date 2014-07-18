62c95e4efb222edc70d98ed5327447c4cc9daa86
C.W. Betts <computers57@hotmail.com>
2014-07-18 16:59:40 -0600

    Add a few more classes.

---
+++ PlayerPRO 6/ClassicalViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ClassicalViewController.swift
+//  PPMacho
+//
+//  Created by C.W. Betts on 7/18/14.
+//
+//
+
+import Cocoa
+
+class ClassicalViewController: NSViewController {
+
+	@IBOutlet var currentDocument: PPDocument
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
---
+++ PlayerPRO 6/WaveViewController.swift
@@ -0,0 +1,20 @@
+//
+//  WaveViewController.swift
+//  PPMacho
+//
+//  Created by C.W. Betts on 7/18/14.
+//
+//
+
+import Cocoa
+
+class WaveViewController: NSViewController {
+
+	@IBOutlet var currentDocument: PPDocument
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
