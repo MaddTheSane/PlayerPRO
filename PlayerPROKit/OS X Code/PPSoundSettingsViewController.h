db7969efae61b19df139d47018f5b864e5499415
C.W. Betts <computers57@hotmail.com>
2014-08-11 16:30:27 -0600

    Back and forth, back and forth...

--- PlayerPROKit/OS X Code/PPSoundSettingsViewController.h
+++ PlayerPROKit/OS X Code/PPSoundSettingsViewController.h
@@ -10,6 +10,10 @@
 #import <Foundation/NSObject.h>
 #import <AppKit/NSViewController.h>
 
+#ifndef NS_DESIGNATED_INITIALIZER
+#define NS_DESIGNATED_INITIALIZER
+#endif
+
 @protocol PPSoundSettingsViewControllerDelegate <NSObject>
 - (void)soundOutBitsDidChange:(short)bits;
 - (void)soundOutRateDidChange:(unsigned int)rat;
@@ -41,7 +45,9 @@
 @property (weak) IBOutlet NSPopUpButton *reverbPercent;
 @property (weak) IBOutlet id<PPSoundSettingsViewControllerDelegate> delegate;
 
++ (instancetype)newSoundSettingWindow NS_RETURNS_RETAINED;
 - (instancetype)init;
+- (instancetype)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil NS_DESIGNATED_INITIALIZER;
 - (void)settingsFromDriverSettings:(MADDriverSettings*)sett;
 
 - (IBAction)changeBits:(id)sender;
