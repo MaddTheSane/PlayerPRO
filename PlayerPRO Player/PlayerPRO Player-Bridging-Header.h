a7a4940a35e89479685a651d3c76a7b4b36fe23c
C.W. Betts <computers57@hotmail.com>
2014-11-24 19:29:16 -0700

    Remove unneeded stuff from the Player's bridging header.

--- PlayerPRO Player/PlayerPRO Player-Bridging-Header.h
+++ PlayerPRO Player/PlayerPRO Player-Bridging-Header.h
@@ -2,9 +2,6 @@
 //  Use this file to import your target's public headers that you would like to expose to Swift.
 //
 
-#include <PlayerPROCore/PlayerPROCore.h>
-#include "PPByteswap.h"
-
 #import "UserDefaultKeys.h"
 #import "PPSTImporter.h"
 #import "AppDelegate.h"
