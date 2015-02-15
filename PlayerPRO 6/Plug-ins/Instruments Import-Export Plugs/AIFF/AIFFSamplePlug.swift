a15558e06596d429fafdc6ac4aa8cba6bf3bf270
C.W. Betts <computers57@hotmail.com>
2015-02-15 11:01:44 -0700

    PlayerPROKit: Hopefully better usage of the audio APIs in AIFF. Swap bytes in small chunks, instead of all at once: This will make the memory footprint smaller when exporting.

--- PlayerPRO 6/Plug-ins/Instruments Import-Export Plugs/AIFF/AIFFSamplePlug.swift
+++ PlayerPRO 6/Plug-ins/Instruments Import-Export Plugs/AIFF/AIFFSamplePlug.swift
@@ -11,7 +11,7 @@ import AudioToolbox
 import PlayerPROKit
 import SwiftAdditions
 
-public final class AIFFSamplePlug: NSObject, PPSampleExportPlugin {
+public final class AIFFSamplePlug: NSObject, PPSampleExportPlugin /*, PPSampleImportPlugin*/ {
 	public let hasUIForExport = false
 	
 	override init() {
@@ -26,48 +26,52 @@ public final class AIFFSamplePlug: NSObject, PPSampleExportPlugin {
 		var asbd = AudioStreamBasicDescription(sampleRate: Float64(sample.c2spd), formatID: .LinearPCM, formatFlags: .SignedInteger | .Packed | .BigEndian, bitsPerChannel: UInt32(sample.amplitude), channelsPerFrame: sample.stereo ? 2 : 1)
 		
 		let datLen = sample.data.length
-		
-		var data: UnsafePointer<Int8>
-		
 		var audioFile: AudioFileID = nil
-		var res: OSStatus = noErr
 		var myErr = MADErr.NoErr
 		
-		if sample.amplitude == 16 {
-			var tmpData = UnsafeMutablePointer<Int16>(malloc(UInt(datLen)))
-			if tmpData == nil {
-				return .NeedMemory
-			}
-			memcpy(tmpData, sample.data.bytes, UInt(datLen))
-			dispatch_apply(UInt(datLen) / 2, dispatch_get_global_queue(0, 0), { (i) -> Void in
-				let ii = Int(i)
-				tmpData[ii] = tmpData[ii].bigEndian
-			})
-			data = UnsafePointer<Int8>(tmpData)
-		} else {
-			data = UnsafePointer<Int8>(sample.data.bytes)
-		}
-		
-		res = AudioFileCreate(URL: sampleURL, fileType: .AIFF, format: &asbd, flags: .EraseFile, audioFile: &audioFile)
+		var res = AudioFileCreate(URL: sampleURL, fileType: .AIFF, format: &asbd, flags: .EraseFile, audioFile: &audioFile)
 		
 		if res != noErr {
-			myErr = .WritingErr
+			return .WritingErr
 		} else {
 			var numBytes = UInt32(datLen)
 			
-			res = AudioFileWriteBytes(audioFile, false, 0, &numBytes, data);
+			if sample.amplitude == 16 {
+				if isBigEndian {
+					res = AudioFileWriteBytes(audioFile, false, 0, &numBytes, sample.data.bytes);
+				} else {
+					let maxWriteBytes = 16
+					var remaining = datLen
+					var location = 0
+					var toWriteSize = 16
+					var toWriteBytes = [Int16](count: 8, repeatedValue: 0)
+					let tmpData = UnsafePointer<Int16>(sample.data.bytes)
+					do {
+						for i in 0..<(toWriteSize / 2) {
+							toWriteBytes[i] = tmpData[location / 2 + i].bigEndian
+						}
+						remaining -= toWriteSize
+						var tmpToWrite = UInt32(toWriteSize)
+						res = AudioFileWriteBytes(audioFile, true, Int64(location), &tmpToWrite, toWriteBytes)
+						location += toWriteSize
+						toWriteSize = min(remaining, maxWriteBytes)
+					} while toWriteSize != 0 && res == noErr
+					if res != noErr {
+						AudioFileClose(audioFile)
+						return .WritingErr
+					}
+				}
+			} else {
+				res = AudioFileWriteBytes(audioFile, false, 0, &numBytes, sample.data.bytes);
+			}
 			if (res != noErr) {
 				myErr = .WritingErr;
 			}
 			
 			res = AudioFileClose(audioFile);
 			if (res != noErr) {
-				myErr = .WritingErr;
+				return .WritingErr
 			}
-		}
-		
-		if (sample.amplitude == 16) {
-			free(UnsafeMutablePointer<Void>(data))
 		}
 		
 		return myErr
@@ -95,5 +99,9 @@ public final class AIFFSamplePlug: NSObject, PPSampleExportPlugin {
 		}
 		
 		return myErr == .NoErr
+	}
+	
+	func importSampleAtURL(sampleURL: NSURL!, sample: AutoreleasingUnsafeMutablePointer<PPSampleObject?>, driver: PPDriver!) -> MADErr {
+		return .OrderNotImplemented
 	}
 }
