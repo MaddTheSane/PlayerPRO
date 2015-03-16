7e6e127d01036a5a9476ef8781f43f92442f691b
C.W. Betts <computers57@hotmail.com>
2015-03-15 19:00:35 -0600

    Move the error testing to its own file, separate from the main tester. Add a simple MADK file to test.

--- PlayerPROKit Tests/PlayerPROKit_Tests.swift
+++ PlayerPROKit Tests/PlayerPROKit_Tests.swift
@@ -11,76 +11,39 @@ import XCTest
 import PlayerPROCore
 import PlayerPROKit
 
-func GlobalDebugStr(line: Int16, file: UnsafePointer<Int8>, text: UnsafePointer<Int8>) {
-	let manager = NSFileManager.defaultManager()
-	let fileStr = manager.stringWithFileSystemRepresentation(file, length: Int(strlen(file)))
-	let textStr = String(UTF8String: text)!
-
-	println("\(fileStr):\(line) Global Test: \(textStr)")
-}
-
 class PlayerPROKit_Tests: XCTestCase {
 	var ourBundle: NSBundle? = nil
 	var ourLib: PPLibrary? = nil
-	var exampleVar = ""
 	
     override func setUp() {
         super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
 		ourBundle = NSBundle(forClass: PlayerPROKit_Tests.self)
-		MADRegisterDebugBlock(nil)
 		if let ourPlugPath = ourBundle?.builtInPlugInsURL {
 			ourLib = PPLibrary(plugInURL: ourPlugPath)
 		} else {
 			ourLib = PPLibrary()
 		}
-		var exampleVar = ""
     }
 	
     override func tearDown() {
 		ourLib = nil
-		MADRegisterDebugBlock(nil)
         // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
     }
 
-	func classDebugStr(line: Int16, file: UnsafePointer<Int8>, text: UnsafePointer<Int8>) {
-		let manager = NSFileManager.defaultManager()
-		let fileStr = manager.stringWithFileSystemRepresentation(file, length: Int(strlen(file)))
-		XCTAssertEqual(fileStr, __FILE__, "files aren't equal...")
-		var textStr = String.fromCString(text)!
-		textStr += ", var: \"\(exampleVar)\""
-		
-		println("\(fileStr):\(line) Class Test: \(textStr)")
-	}
-	
-	func testInlineDebugBlock() {
-		MADRegisterDebugBlock { (line, file, text) -> Void in
-			let manager = NSFileManager.defaultManager()
-			let fileStr = manager.stringWithFileSystemRepresentation(file, length: Int(strlen(file)))
-			let textStr = String(UTF8String: text)!
-			
-			println("\(fileStr):\(line) Inline Test: \(textStr)")
-		}
-		MADDebugString("Swift Test")
-		MADDebugStr(__LINE__, __FILE__, "C-Style test")
-	}
-	
-	func testGlobalDebugBlock() {
-		MADRegisterDebugBlock(GlobalDebugStr)
-		MADDebugString("Swift Test")
-		MADDebugStr(__LINE__, __FILE__, "C-Style test")
-	}
-	
-	func testClassDebugBlock() {
-		MADRegisterDebugBlock(classDebugStr)
-		MADDebugString("Swift Test")
-		MADDebugStr(__LINE__, __FILE__, "C-Style test")
-		exampleVar = "Variable 1"
-		MADDebugString("Swift Test")
-		MADDebugStr(__LINE__, __FILE__, "C-Style test")
-		exampleVar = "Variable 2"
-		MADDebugString("Swift Test")
-		MADDebugStr(__LINE__, __FILE__, "C-Style test")
+	func testInformation() {
+		let musicPath = ourBundle!.URLForResource("TestMADK", withExtension: "madk")!
+		let info = ourLib!.informationFromFile(URL: musicPath, type: "MADK")
+		XCTAssert(info.error == .NoErr)
+		let unwrapped = info.info!
+		println(unwrapped)
+		XCTAssertEqual("MADK", unwrapped.signature)
+		XCTAssertEqual(unwrapped.totalPatterns, 7)
+		XCTAssertEqual(unwrapped.partitionLength, 14)
+		XCTAssertEqual(unwrapped.fileSize, 83671)
+		XCTAssertEqual(unwrapped.totalTracks, 6)
+		XCTAssertEqual(unwrapped.internalFileName, "Go For It")
+		XCTAssertEqual(unwrapped.formatDescription, "MADK")
 	}
 }
