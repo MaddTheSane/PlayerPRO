926cc9b77b7878a215e2cefdfac026b950db1bb5
C.W. Betts <computers57@hotmail.com>
2016-04-14 00:30:45 -0600

    Minor poke.

--- PlayerPRO-OSX.playground/Contents.swift
+++ PlayerPRO-OSX.playground/Contents.swift
@@ -1,8 +1,45 @@
 //: Playground - noun: a place where people can play
 
 import Cocoa
+import SwiftAdditions
 import PlayerPROCore
 import PlayerPROKit
+
+let aSet: Set<Int> = [0,4,3,6,5,5,3,0,0,1,1,1,1,1,1]
+let anArray = [0,4,3,6,5,5,3, 0,0]
+let idxSet: NSIndexSet = {
+	let toRet = NSMutableIndexSet()
+	for i in [0,4,3,6,5,5,3, 0,0] {
+		toRet.addIndex(i)
+	}
+	return toRet
+}()
+
+let setIdxSet = NSIndexSet(indexes: aSet)
+let arrIdxSet = NSIndexSet(indexes: anArray)
+
+idxSet == setIdxSet
+idxSet == arrIdxSet
+print(idxSet)
+(idxSet as! NSMutableIndexSet).addIndex(1)
+print(idxSet)
+idxSet == setIdxSet
+idxSet == arrIdxSet
+
+let arr: [String] = {
+	var inArr = Array(2.stride(through: 17, by: 2))
+	
+	return Array(inArr.map({return String($0)}))
+}()
+
+var arr1 = arr
+var arr2 = arr
+var arr3 = arr
+
+arr1.removeAtIndexes(anArray)
+arr2.removeAtIndexes(arrIdxSet)
+
+arr1 == arr2
 
 print(MadID)
 
@@ -16,5 +53,3 @@ bSetting.resetToBestDriver()
 aSetting == bSetting
 
 print(aSetting)
-
-var str = "Hello, playground"
