602371c275d16b7394600be88115d02340c184d2
C.W. Betts <computers57@hotmail.com>
2018-06-03 23:43:01 -0600

    Better Swift naming conventions for the waveform image generation.

--- PlayerPRO 6/Document classes/InstrumentPanelController.swift
+++ PlayerPRO 6/Document classes/InstrumentPanelController.swift
@@ -194,7 +194,7 @@ class InstrumentPanelController: NSWindowController, NSOutlineViewDataSource, NS
 				self.instrumentNote.stringValue = octaveName(fromNote: UInt8(untmpObj.realNote), options: [.useSharpSymbol]) ?? "---"
 				self.instrumentBits.stringValue = untmpObj.amplitude != 0 ? "\(untmpObj.amplitude)-bit" : PPDoubleDash
 				self.instrumentMode.stringValue = untmpObj.loopType == .pingPong ? "Ping-pong" : "Classic"
-				let tmpIm = untmpObj.waveformImage(view: self.waveFormImage)
+				let tmpIm = untmpObj.waveformImage(using: self.waveFormImage)
 				self.waveFormImage.image = tmpIm
 			}
 		}
